53331ea2f87722282d03fdd592713923d5b23e3f
unknown <davin.aristia81@gmail.com>
2022-03-21 16:39:56 +0700

    second commit

--- db/index.js
+++ db/index.js
@@ -1,1 +1,12 @@
- 
+ const mongoose = require('mongoose')
+ const { urlDb } = require('../config')
+
+ mongoose.connect(urlDb, {
+     useUnifiedTopology: true
+         //  useFineAndModify: true,
+         //  useCreateIndex: true
+ })
+
+ const db = mongoose.connection
+
+ module.exports = db
